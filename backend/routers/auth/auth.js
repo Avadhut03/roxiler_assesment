afc91be24d929366ad8b3eeb5e5e3cf14c505daa
Avadhut03 <avadhutkul60@gmail.com>
2025-09-19 23:37:13 +0530

    Update Pssword Route

--- backend/routers/auth/auth.js
+++ backend/routers/auth/auth.js
@@ -52,5 +52,31 @@ router.post("/logout", (req, res) => {
   res.json({ message: "Logged out (delete token on client)" });
 });
 
+router.put("/password",async(req,res)=>{
+    try {
+        const {oldPassword, newPassword}= req.body;
+        const authHeader = req.headers.authorization;
+        if(!authHeader){
+            return res.status(401).json({error:"Authorization header missing"});
+        }
+        const token = authHeader.split(" ")[1];
+        const decoded= jwt.verify(token, process.env.JWT_SECRET);
+        console.log(decoded)
+        const user = await prisma.user.findUnique({where:{id:decoded.id}});
+        if(!user){
+            return res.status(404).json({error:"User not found"});
+        }
+        if(!newPassword.match(/^(?=.*[A-Z])(?=.*[!@#$%^&*]).{8,16}$/)){
+            return res.status(400).json({error: "Password must be between 8 to 16 characters and contain at least one uppercase letter and one special character"});
+        }   
+        await prisma.user.update({
+            where:{id:user.id},
+            data:{password:newPassword}
+        })
+        res.json({message:"Password updated successfully"});
+    } catch (error) {
+        console.error(error);
+    }
+})
 
 module.exports= router;
